8405bb5163d0497f8a513f4535dba44b22fa758d
zipo1964 <104211627+zipo1964@users.noreply.github.com>
2025-10-06 20:34:55 +0400

    Fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
- 
+  base: process.env.VITE_BASE_PATH || "/react-vite-project",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
